37aeaa0b2d078d01880b7876bcb3380408cd6321
ChVictoria <butterflylight01@gmail.com>
2023-10-09 10:13:06 +0300

    get rid of global variable to make it possible to create several linked lists

--- task3.c
+++ task3.c
@@ -21,24 +21,24 @@ typedef struct item
     struct item *nextBook;
 }item;
 
-item *head = NULL;
 
-void insertEnd(book book) {
+
+void insertEnd(book book, item **head) {
     item *lastBook = (item *) malloc(sizeof(item));
     lastBook->harryPotterBook = book;
     lastBook->nextBook = NULL;
-    struct item *catalog = head;
-    if (head != NULL) {
+    struct item *catalog = *head;
+    if (*head != NULL) {
         while (catalog->nextBook != NULL) {
             catalog = catalog->nextBook;
         }
         catalog->nextBook = lastBook;
     }else{
-        head = lastBook;
+        *head = lastBook;
     }
 }
 
-void printCatalog(){
+void printCatalog(item *head){
     item *currentBook = head;
     unsigned int i = 1;
     while(currentBook != NULL) {
@@ -64,11 +64,12 @@ void printCatalog(){
 }
 
 int main(){
+    item *head = NULL;
     book book1 = {"Philosopher's Stone", 10.35,223,"english",379.884,2014};
     book book2 = {"Chamber of Secrets", 21.98,341,"english",1587.57,2016};
     book book3 = {"Prisoner of Azkaban", 27.285,383,"ukrainian",370,2017};
-    insertEnd(book1);
-    insertEnd(book2);
-    insertEnd(book3);
-    printCatalog();
+    insertEnd(book1, &head);
+    insertEnd(book2, &head);
+    insertEnd(book3, &head);
+    printCatalog(head);
 }
